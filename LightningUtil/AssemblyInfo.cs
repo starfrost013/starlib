89402016b53a5c40cf09280fce05d3352ce41fa8
starfrost013 <mario64crashed@gmail.com>
2023-05-21 21:10:21 +0100

    sane versioning

---
+++ LightningUtil/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+﻿[assembly: AssemblyDescription("Starlib.NET Utilities")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]      
--- LightningUtil/Tools/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-﻿[assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0.0")]
-[assembly: AssemblyTitle("The Lightning Game Engine - Utilities")]
